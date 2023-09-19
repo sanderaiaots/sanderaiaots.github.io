9f98731a9acd667b08fbc0c2538382d1a5766ed3
Sander Aiaots <sander.aiaots@helmes.com>
2023-09-19 16:59:44 +0300

    tuleviku tunnihinnad koos võrgutasuga

---
+++ _app/immutable/chunks/4.00fd75b5.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page.da46b06b.js";import{default as t}from"../entry/future-page.svelte.0eda348a.js";export{t as component,r as universal};
---
+++ _app/immutable/chunks/5.e1778678.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page.fca24c55.js";import{default as t}from"../entry/hind-page.svelte.29ff6f40.js";export{t as component,r as universal};
